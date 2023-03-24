929c134c25461780ae662102206ddf50a9807f3d
unknown <chandrashanmugam007@gmail.com>
2023-03-24 10:35:46 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Caitlyn_chocolates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Caitlyn Chocolates: ");
+
+            Program p = new Program();
+            List<string> chocolatesDispensor = new List<string>();
+
+
+
+        }
+
+        void addChocolates(List<string> chocoDispensor,string color,int count)
+        {
+            for(int i = 0;i<count;i++)
+            {
+                chocoDispensor.Add(color);
+            }
+        }
+        static string[] removeChocolates(List<string> chocoDispensor,int count)
+        {
+            string[] chocosRemoved = new string[count];
+            for(int i = 0; i < count; i++)
+            {
+                chocosRemoved[i] = chocoDispensor[chocoDispensor.Count - 1]
+                chocoDispensor.Remove(chocoDispensor[chocoDispensor.Count -1]);
+                
+            }
+            return chocosRemoved;
+        }
+        static string[] dispenseChocolates(List<string> chocoDispensor, int count)
+        {
+            string[] chocosDispensed = new string[count];
+            for (int i = 0; i < count; i++)
+            {
+                chocoDispensor.Remove(chocosRemoved[i]);
+
+            }
+            return chocosDispensed;
+        }
+
+
+
+
+    }
+}
